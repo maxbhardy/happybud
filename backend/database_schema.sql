0be6d3fcd9d39e1557bd58190c1041407101a444
Hamza <hamza22zirar@gmail.com>
2025-03-27 13:27:46 -0400

    the database sql file

--- backend/database_schema.sql
+++ backend/database_schema.sql
@@ -1,5 +1,5 @@
-PRAGMA journal_mode=WAL;
-PRAGMA foreign_keys=ON;
+PRAGMA journal_mode = WAL;
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS Plants (
     PlantID INTEGER PRIMARY KEY,
@@ -10,11 +10,12 @@ CREATE TABLE IF NOT EXISTS Plants (
 
 CREATE TABLE IF NOT EXISTS PlantClasses (
     PlantClassID INTEGER PRIMARY KEY,
-    PlantID INTEGER REFERENCES Plants,
+    PlantID INTEGER,
     ClassCode TEXT,
     ClassName TEXT,
     ClassDescription TEXT,
-    ClassIdentification TEXT
+    ClassIdentification TEXT,
+    FOREIGN KEY (PlantID) REFERENCES Plants(PlantID)
 );
 
 CREATE TABLE IF NOT EXISTS PlantSolutions (
@@ -26,7 +27,48 @@ CREATE TABLE IF NOT EXISTS PlantSolutions (
 );
 
 CREATE TABLE IF NOT EXISTS ClassSolutionRelationships (
-    PlantClassID INTEGER REFERENCES PlantClasses,
-    SolutionID INTEGER REFERENCES PlantSolutions,
-    PRIMARY KEY (PlantClassID, SolutionID)
+    PlantClassID INTEGER,
+    SolutionID INTEGER,
+    PRIMARY KEY (PlantClassID, SolutionID),
+    FOREIGN KEY (PlantClassID) REFERENCES PlantClasses(PlantClassID),
+    FOREIGN KEY (SolutionID) REFERENCES PlantSolutions(SolutionID)
 ) WITHOUT ROWID;
+
+CREATE TABLE IF NOT EXISTS Produits (
+    ProduitId INTEGER PRIMARY KEY,
+    ProductName Text,
+    Description Text,
+    Fournisseur TEXT,
+    Lieu TEXT,
+    Prix REAL,
+);
+CREATE TABLE IF NOT EXISTS PlantSolutionRelationships (
+    SolutionID INTEGER,
+    ProduitId INTEGER,
+    PRIMARY KEY (SolutionID, ProduitId),
+    FOREIGN KEY (ProduitId) REFERENCES Produits(ProduitId),
+    FOREIGN KEY (SolutionID) REFERENCES PlantSolutions(SolutionID)
+) WITHOUT ROWID;
+
+
+CREATE TABLE IF NOT EXISTS Historique (
+    HistoriqueId INTEGER PRIMARY KEY,
+    PlantID INTEGER,
+    PlantClassID INTEGER,
+    PictureURI TEXT,
+    ThumbnailURI TEXT,
+    Timestamp TEXT,
+    FOREIGN KEY (PlantID) REFERENCES Plants(PlantID),
+    FOREIGN KEY (PlantClassID) REFERENCES PlantClasses(PlantClassID)
+);
+
+CREATE TABLE IF NOT EXISTS HistoriqueResult (
+    ResultId INTEGER PRIMARY KEY,
+    HistoriqueId INTEGER,
+    PlantClassID INTEGER,
+    result REAL,
+    FOREIGN KEY (HistoriqueId) REFERENCES Historique(HistoriqueId),
+    FOREIGN KEY (PlantClassID) REFERENCES PlantClasses(PlantClassID)
+);
+
+
