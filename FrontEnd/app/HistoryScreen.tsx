1eb1862fb0d364f45f87d7941beb19e7ed792cf8
Jin1411 <aurelian.haffner@gmail.com>
2025-04-23 15:21:39 -0400

    feat : added database integration to historique page

--- FrontEnd/app/HistoryScreen.tsx
+++ FrontEnd/app/HistoryScreen.tsx
@@ -4,23 +4,49 @@ import BottomNav from "@/components/BottomNav";
 import { StatusBar } from "expo-status-bar";
 import HistoriqueComp from "@/components/HistoriqueComp";
 import SearchBar from "@/components/SearchBar";
+import { getHistoriqueResults } from "@/utils/database";
+import { useState, useEffect } from "react";
+import useDatabase from "@/hooks/useDatabase";
+
+
 export default function HistoryScreen() {
-  const data = [
-    {
-      id: 1,
-      title: "Tomate - Ravageur",
-      date: "Aujourd'hui",
-      description:
-        "Petite description mais celui-ci est grand pour montrer l'utilité de la ...",
-    },
-    {
-      id: 2,
-      title: "Tomate - Plant saint",
-      date: "Il y a 3 jours",
-      description:
-        "Description plus courte, tout va bien, aucune anomalie détectée.",
-    },
-  ];
+  const [thumbnailURI, setThumbnailURI] = useState<Array<string | null>>([]);
+  const [historiqueId, setHistoriqueId] = useState<Array<string | null>>([]);
+  const [plantName, setPlantName] = useState<Array<string | null>>([]);
+  const [timestamp, setTimestamp] = useState<Array<string | null>>([]);
+  const [plantClassName, setPlantClassName] = useState<Array<string | null>>([]);
+  const [data, setData] = useState<Array<any>>([]);
+  const db = useDatabase();
+
+
+
+  const getHistorique = async () => {
+    var historiques = await db.getAllAsync("SELECT * FROM Historique ORDER BY Timestamp DESC");
+
+    for (const row of historiques) {
+      console.log(row.HistoriqueId);
+
+      // const hist_res = db.getFirstAsync("SELECT * FROM HistoriqueResult WHERE HistoriqueId == ?",row.HistoriqueId);
+      const plant_name = db.getFirstAsync("SELECT PlantName FROM Plants WHERE PlantID = ?", row.PlantID);
+
+      const plant_class_name = db.getFirstAsync("SELECT ClassName FROM PlantClasses WHERE PlantClassID = ?", row.PlantClassID);
+
+      historiques.plant_name = plant_name;
+      historiques.plant_class_name = plant_class_name;
+      //      thumbnailURI.push(row?.ThumbnailURI);
+      //      plantName.push(plant_name);
+      //      historiqueId.push(row?.HistotriqueId);
+      //      timestamp.push(row?.Timestamp);
+      //      plantClassName.push(plant_class_info?.ClassName);
+      setData(historiques);
+    }
+
+  };
+
+  useEffect(() => {
+    getHistorique(); // Call the async function when the component mounts
+  }, [db]);
+
   return (
     <SafeAreaView className="flex-1 bg-[#DFD8D1]">
       <StatusBar style="auto" />
@@ -33,10 +59,10 @@ export default function HistoryScreen() {
       <View className="my-10">
         {data.map((item) => (
           <HistoriqueComp
-            key={item.id}
-            title={item.title}
-            date={item.date}
-            description={item.description}
+            key={item.HistoriqueId}
+            title={item.plant_name}
+            date={item.Timestamp}
+            description={item.plant_class_name}
             onPress={() => {
               // Par exemple, navigation vers un écran de détail
               // navigation.navigate('HistoriqueDetail', { id: item.id });
